c2980fe74f89c5279c463a5856807a5e6a5dd626
Delaskar <delaskarcai@gmail.com>
2023-10-08 13:27:02 +0300

    New Function.

--- src/data_processor/subdomain_health.rs
+++ src/data_processor/subdomain_health.rs
@@ -1,47 +1,4 @@
-use reqwest::blocking;
-use reqwest::Error as Err;
-use reqwest::StatusCode;
-use serde_json;
-use std::collections::{HashMap, HashSet};
+// use reqwest::blocking;
+// use reqwest::Error;
 
-pub fn subdomains_status(
-    subdomains_list: &Vec<String>,
-) -> Result<HashMap<String, StatusCode>, Err> {
-    // To track success subdomains.
-    let mut subdomains_status: HashMap<String, StatusCode> = HashMap::new();
-    // To track failed subdomains.
-    let mut failed_subdomains: HashSet<String> = HashSet::new();
 
-    for subdomain in subdomains_list {
-        let subdomain_http = format!("https://{}", subdomain); //add "https" protocol
-        println!("Verifying URL: {}", subdomain_http);
-        match blocking::get(subdomain_http) {
-            Ok(response) => {
-                let status = response.status();
-                subdomains_status.insert(subdomain.clone(), status);
-
-                // If the status code is not successful (2xx), register the subdomain as failed
-                if !status.is_success() {
-                    failed_subdomains.insert(subdomain.clone());
-                }
-            }
-            Err(err) => {
-                eprintln!("Error verifying {}: {:?}", subdomain, err);
-                // You can handle errors here, such as logging or tracking them.
-                failed_subdomains.insert(subdomain.clone());
-            }
-        }
-    }
-    // Report failed subdomains.
-    if !failed_subdomains.is_empty() {
-        // Serialize errors in JSON format and display them.
-        let error_info: Vec<(&String, &str)> = failed_subdomains
-            .iter()
-            .map(|subdomain| (subdomain, "Request error!"))
-            .collect();
-        let error_json =
-            serde_json::to_string_pretty(&error_info).expect("Error serializing errors to JSON");
-        eprintln!("Subdomains that failed:\n{}", error_json);
-    }
-    Ok(subdomains_status)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod data_processor;
 pub use crate::data_processor::file_processing;
-pub use crate::data_processor::subdomain_health;
+// pub use crate::data_processor::subdomain_health;
 use std::path::PathBuf;
 
 fn main() {
@@ -11,7 +11,6 @@ fn main() {
     let path_file: PathBuf = PathBuf::from(&user_path);
 
     let file_domains = file_processing::process_file(&path_file).unwrap();
-    let domain_status = subdomain_health::subdomains_status(&file_domains);
 
-    println!("{:?}", domain_status);
+    println!("{:?}", file_domains);
 }
